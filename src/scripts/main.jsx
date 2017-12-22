bd6e7b150e9a26cb07d9b56c7f888dad4651a78c
Bartzlo <ksb.bart@gmail.com>
2017-12-22 21:26:06 +0500

    Finish slider

--- src/scripts/main.jsx
+++ src/scripts/main.jsx
@@ -7,11 +7,13 @@ class HomePage extends React.Component {
   constructor (props) {
     super(props)
 
-    this.state = {
-      0: this.props.start,
-      1: this.props.start + 1,
-      2: this.props.start - 1
-    }
+    const start = this.props.start
+    let imgsProp = this.props.images.map((item, i) => {
+      return {src: start - i <= 1 && i - start <= 1 ? item.src : ' ', bgSrc: item.src}
+    })
+
+    this.state = {imgsProp: imgsProp}
+
 
     this.onTransactionEnd = this.onTransactionEnd.bind(this)
     this.handleCallback = this.handleCallback.bind(this)
@@ -25,77 +27,132 @@ class HomePage extends React.Component {
   }
 
   handleCallback (index, elem) {
-    let currNum = parseInt(elem.querySelector('img').dataset.currImg)
+    let imgsProp = this.state.imgsProp
+    imgsProp[index + 1].src = imgsProp[index + 1].bgSrc
+    imgsProp[index - 1].src = imgsProp[index - 1].bgSrc
 
-    this.setState({
-      [this.swipe.getPos()]: currNum,
-      [this.getNextPos()]: currNum + 1,
-      [this.getPrevPos()]: currNum - 1
-    })
+    this.setState({imgsProp: imgsProp})
   }
 
   handleClick (event) {
   }
 
-  getPrevPos () {
-    if (this.swipe.getPos() > 0) return this.swipe.getPos() - 1
-    else return 2
-  }
-
-  getNextPos () {
-    if (this.swipe.getPos() < 2) return this.swipe.getPos() + 1
-    else return 0
-  }
-
   render () {
-    const swipeItem0 = (
-      <SwipeItem
-        className={'custom-swipe-item-class'}
-        onClick={this.handleClick}>
-        <img src={this.props.images[this.state['0']].src} alt="" data-curr-img={this.state['0']}/>
-      </SwipeItem>
-    )
+    const swipeItems = this.state.imgsProp.map((imgInfo, i) => {
+      return (
+        <SwipeItem
+          className={'custom-swipe-item-class'}
+          onClick={this.handleClick}
+          key={i.toString()}>
+          <img src={imgInfo.src} />
+        </SwipeItem>
+      )
+    })
 
-    const swipeItem1 = (
-      <SwipeItem
-        className={'custom-swipe-item-class'}
-        onClick={this.handleClick}>
-        <img src={this.props.images[this.state['1']].src} alt="" data-curr-img={this.state['1']}/>
-      </SwipeItem>
-    )
-
-    const swipeItem2 = (
-      <SwipeItem
-        className={'custom-swipe-item-class'}
-        onClick={this.handleClick}>
-        <img src={this.props.images[this.state['2']].src} alt="" data-curr-img={this.state['2']}/>
-      </SwipeItem>
-    )
+    console.log(swipeItems[this.props.start].SwipeImg);
 
     return (
       <Swipe
         className='custom-swipe-container-class'
         ref={o => this.swipe = o}
-        startSlide={0}
+        startSlide={this.props.start}
         speed={300}
         auto={0}
         draggable={true}
-        continuous={true}
+        continuous={false}
         autoRestart={false}
         disableScroll={false}
         stopPropagation={false}
         callback={this.handleCallback}
         transitionEnd={this.onTransactionEnd}>
 
-        {swipeItem0}
-        {swipeItem1}
-        {swipeItem2}
+        {swipeItems}
       </Swipe>
     )
   }
 }
 
 const IMAGES = [
+  {src: './img/0.jpg'},
+  {src: './img/1.jpg'},
+  {src: './img/2.jpg'},
+  {src: './img/3.jpg'},
+  {src: './img/4.jpg'},
+  {src: './img/5.jpg'},
+  {src: './img/6.jpg'},
+  {src: './img/7.jpg'},
+  {src: './img/8.jpg'},
+  {src: './img/9.jpg'},
+  {src: './img/10.jpg'},
+  {src: './img/11.jpg'},
+  {src: './img/12.jpg'},
+  {src: './img/13.jpg'},
+  {src: './img/14.jpg'},
+  {src: './img/15.jpg'},
+  {src: './img/16.jpg'},
+  {src: './img/17.jpg'},
+  {src: './img/18.jpg'},
+  {src: './img/19.jpg'},
+  {src: './img/0.jpg'},
+  {src: './img/1.jpg'},
+  {src: './img/2.jpg'},
+  {src: './img/3.jpg'},
+  {src: './img/4.jpg'},
+  {src: './img/5.jpg'},
+  {src: './img/6.jpg'},
+  {src: './img/7.jpg'},
+  {src: './img/8.jpg'},
+  {src: './img/9.jpg'},
+  {src: './img/10.jpg'},
+  {src: './img/11.jpg'},
+  {src: './img/12.jpg'},
+  {src: './img/13.jpg'},
+  {src: './img/14.jpg'},
+  {src: './img/15.jpg'},
+  {src: './img/16.jpg'},
+  {src: './img/17.jpg'},
+  {src: './img/18.jpg'},
+  {src: './img/19.jpg'},
+  {src: './img/0.jpg'},
+  {src: './img/1.jpg'},
+  {src: './img/2.jpg'},
+  {src: './img/3.jpg'},
+  {src: './img/4.jpg'},
+  {src: './img/5.jpg'},
+  {src: './img/6.jpg'},
+  {src: './img/7.jpg'},
+  {src: './img/8.jpg'},
+  {src: './img/9.jpg'},
+  {src: './img/10.jpg'},
+  {src: './img/11.jpg'},
+  {src: './img/12.jpg'},
+  {src: './img/13.jpg'},
+  {src: './img/14.jpg'},
+  {src: './img/15.jpg'},
+  {src: './img/16.jpg'},
+  {src: './img/17.jpg'},
+  {src: './img/18.jpg'},
+  {src: './img/19.jpg'},
+  {src: './img/0.jpg'},
+  {src: './img/1.jpg'},
+  {src: './img/2.jpg'},
+  {src: './img/3.jpg'},
+  {src: './img/4.jpg'},
+  {src: './img/5.jpg'},
+  {src: './img/6.jpg'},
+  {src: './img/7.jpg'},
+  {src: './img/8.jpg'},
+  {src: './img/9.jpg'},
+  {src: './img/10.jpg'},
+  {src: './img/11.jpg'},
+  {src: './img/12.jpg'},
+  {src: './img/13.jpg'},
+  {src: './img/14.jpg'},
+  {src: './img/15.jpg'},
+  {src: './img/16.jpg'},
+  {src: './img/17.jpg'},
+  {src: './img/18.jpg'},
+  {src: './img/19.jpg'},
   {src: './img/0.jpg'},
   {src: './img/1.jpg'},
   {src: './img/2.jpg'},
