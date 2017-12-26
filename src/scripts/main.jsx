11ff85c2f25d5abc4bca04a775cb525912efb507
Bartzlo <ksb.bart@gmail.com>
2017-12-26 18:26:12 +0500

    Base example

--- src/scripts/main.jsx
+++ src/scripts/main.jsx
@@ -1,4 +1,28 @@
+import {createStore} from 'redux'
+
 console.log('Start main script')
 console.log('-----------------')
 
 ReactDOM.render(<h1>Hello world!</h1>, document.getElementById('root'))
+
+function reducer (state, action) {
+  switch (action.type) {
+  case 'INCREASE_COUNTER':
+    return {...state, counter: state.counter + 1}
+  case 'RESET_COUNTER':
+    return {...state, counter: 0}
+  default:
+    return state
+  }
+}
+
+const store = createStore(reducer, {counter: 0})
+
+store.subscribe(() => console.log(store.getState()))
+
+store.dispatch({type: 'INCREASE_COUNTER'})
+store.dispatch({type: 'INCREASE_COUNTER'})
+store.dispatch({type: 'INCREASE_COUNTER'})
+store.dispatch({type: 'INCREASE_COUNTER'})
+store.dispatch({type: 'RESET_COUNTER'})
+
